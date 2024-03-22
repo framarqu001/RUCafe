e795acb06484910484761d069606a51e1d0c0cf1
aberlins <aberlinski98@gmail.com>
2024-03-22 00:43:48 -0400

    Sandwich view made, still needs to display price and add item to order.

---
+++ src/main/java/rucafe/gui/SandwichController.java
@@ -0,0 +1,106 @@
+package rucafe.gui;
+
+import javafx.fxml.FXML;
+import javafx.scene.Scene;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+
+import java.util.Objects;
+
+public class SandwichController {
+    private Stage primaryStage;
+    private Scene primaryScene;
+    private RUCafeMainController mainController;
+
+    @FXML
+    private ImageView iv_Sandwich;
+
+    @FXML
+    private CheckBox chk_cheese, chk_lettuce, chk_onions, chk_tomatoes;
+
+    @FXML
+    private ComboBox<Sandwich.Bread> cmb_Bread;
+
+    @FXML
+    private ComboBox<Sandwich.Protein> cmb_Protein;
+
+    @FXML
+    private TextField tf_price;
+
+    //Image source: https://www.southernliving.com/ham-sandwich-7971753
+    private final String SANDWICH_PATH = "sandwichPic.jpg";
+    private Sandwich sandwich;
+
+    private void setSandwich() {
+        if(sandwich == null)
+            sandwich = new Sandwich();
+    }
+
+    public void setPrimaryStage(Stage stage, Scene scene) {
+        primaryStage = stage;
+        primaryScene = scene;
+
+        int height = (int) iv_Sandwich.getFitHeight();
+        int width = (int) iv_Sandwich.getFitWidth();
+
+        Image img_Sandwich = new Image(Objects.requireNonNull(getClass().getResourceAsStream(SANDWICH_PATH)),
+                width, height, false, false);
+        iv_Sandwich.setImage(img_Sandwich);
+
+        cmb_Bread.setItems(Sandwich.getBreadList());
+        cmb_Protein.setItems(Sandwich.getProteinList());
+        setSandwich();
+    }
+
+    @FXML
+    protected void setProtein() {
+        sandwich.setProtein(cmb_Protein.getValue());
+    }
+
+    @FXML
+    protected void setBread() {
+        sandwich.setBread(cmb_Bread.getValue());
+    }
+
+    @FXML
+    protected void addCheese() {
+        sandwich.addAddOn(Sandwich.AddOn.CHEESE);
+    }
+
+    @FXML
+    protected void addLettuce() {
+        sandwich.addAddOn(Sandwich.AddOn.LETTUCE);
+    }
+
+    @FXML
+    protected void addTomatoes() {
+        sandwich.addAddOn(Sandwich.AddOn.TOMATOES);
+    }
+
+    @FXML
+    protected void addOnions() {
+        sandwich.addAddOn(Sandwich.AddOn.ONIONS);
+    }
+
+    @FXML
+    protected void clear() {
+        sandwich = new Sandwich();
+        cmb_Bread.valueProperty().set(null);
+        cmb_Protein.valueProperty().set(null);
+
+        chk_cheese.setSelected(false);
+        chk_lettuce.setSelected(false);
+        chk_onions.setSelected(false);
+        chk_tomatoes.setSelected(false);
+    }
+
+    @FXML
+    protected void displayMain() {
+        primaryStage.setScene(primaryScene);
+    }
+
+}
--- src/main/java/rucafe/gui/SandwichTest.java
+++ src/main/java/rucafe/gui/SandwichTest.java
@@ -7,63 +7,63 @@ import org.junit.Test;
 public class SandwichTest {
 
     private Sandwich beef, tuna, chicken, undecided;
-    private final Sandwich.AddIn [] ADD_INS = Sandwich.AddIn.values();
+    private final Sandwich.AddOn [] ADD_ONS = Sandwich.AddOn.values();
     private static final double DELTA = 0.00000001;
 
     @Before
     public void setup(){
-        beef = new Sandwich(Sandwich.SandwichType.BEEF, Sandwich.Bread.BAGEL);
-        tuna = new Sandwich(Sandwich.SandwichType.FISH, Sandwich.Bread.SOURDOUGH);
-        chicken = new Sandwich(Sandwich.SandwichType.CHICKEN, Sandwich.Bread.WHEAT);
+        beef = new Sandwich(Sandwich.Protein.BEEF, Sandwich.Bread.BAGEL);
+        tuna = new Sandwich(Sandwich.Protein.FISH, Sandwich.Bread.SOURDOUGH);
+        chicken = new Sandwich(Sandwich.Protein.CHICKEN, Sandwich.Bread.WHEAT);
     }
 
     @Test
-    public void testChickenAddIns() {
+    public void testChickenNoAddOns() {
         Assert.assertEquals(8.99, chicken.price(), DELTA);
     }
 
     @Test
     public void testBeefWithCheese() {
-        beef.addAddIn(Sandwich.AddIn.CHEESE);
+        beef.addAddOn(Sandwich.AddOn.CHEESE);
         Assert.assertEquals(11.99, beef.price(), DELTA);
     }
 
     @Test
     public void testTunaWithOnionsLettuceTomatoes() {
-        tuna.addAddIns(new Sandwich.AddIn[] {Sandwich.AddIn.ONIONS, Sandwich.AddIn.LETTUCE,
-                Sandwich.AddIn.TOMATOES});
+        tuna.addAddOns(new Sandwich.AddOn[] {Sandwich.AddOn.ONIONS, Sandwich.AddOn.LETTUCE,
+                Sandwich.AddOn.TOMATOES});
         Assert.assertEquals(10.89, tuna.price(), DELTA);
     }
 
     @Test
-    public void testChickenWithAllAddIns() {
-        for (Sandwich.AddIn extraAddIns : ADD_INS) {
-            chicken.addAddIn(extraAddIns);
+    public void testChickenWithAllAddOns() {
+        for (Sandwich.AddOn extraAddOns : ADD_ONS) {
+            chicken.addAddOn(extraAddOns);
         }
         Assert.assertEquals(10.89, chicken.price(), DELTA);
     }
 
     @Test
-    public void testRemoveAddIns() {
-        undecided = new Sandwich(Sandwich.SandwichType.BEEF, Sandwich.Bread.WHEAT);
-        undecided.addAddIns(ADD_INS);
-        undecided.removeAddIns(ADD_INS);
+    public void testRemoveAddOns() {
+        undecided = new Sandwich(Sandwich.Protein.BEEF, Sandwich.Bread.WHEAT);
+        undecided.addAddOns(ADD_ONS);
+        undecided.removeAddOns(ADD_ONS);
         Assert.assertEquals(10.99, undecided.price(), DELTA);
     }
 
     @Test
-    public void testRemoveAndReaddAddIns() {
-        undecided = new Sandwich(Sandwich.SandwichType.FISH, Sandwich.Bread.BAGEL);
-        undecided.addAddIn(Sandwich.AddIn.ONIONS);
-        undecided.removeAddIn(Sandwich.AddIn.ONIONS);
-        undecided.addAddIn(Sandwich.AddIn.ONIONS);
+    public void testRemoveAndReaddAddOns() {
+        undecided = new Sandwich(Sandwich.Protein.FISH, Sandwich.Bread.BAGEL);
+        undecided.addAddOn(Sandwich.AddOn.ONIONS);
+        undecided.removeAddOn(Sandwich.AddOn.ONIONS);
+        undecided.addAddOn(Sandwich.AddOn.ONIONS);
         Assert.assertEquals(10.29, undecided.price(), DELTA);
     }
 
     @Test
     public void testChangeSandwichType() {
-        undecided = new Sandwich(Sandwich.SandwichType.CHICKEN, Sandwich.Bread.BAGEL);
-        undecided.setSandwichType(Sandwich.SandwichType.FISH);
+        undecided = new Sandwich(Sandwich.Protein.CHICKEN, Sandwich.Bread.BAGEL);
+        undecided.setProtein(Sandwich.Protein.FISH);
         Assert.assertEquals(9.99, undecided.price(), DELTA);
     }
 }
