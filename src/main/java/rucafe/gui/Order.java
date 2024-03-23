66faffbd8da89c3037099c0e35b22a66fbb653da
framarqu001 <119390184+framarqu001@users.noreply.github.com>
2024-03-23 14:42:11 -0400

    string properties to order

--- src/main/java/rucafe/gui/Order.java
+++ src/main/java/rucafe/gui/Order.java
@@ -18,36 +18,51 @@ public class Order {
     private ObservableList<MenuItem> menuItems;
     private double total; // maybe make into property?
     private SimpleStringProperty totalStringProperty;
+    private SimpleStringProperty subTotalStringProperty;
+    private SimpleStringProperty salesTaxStringProperty;
 
     public Order() {
         orderNumber = ++totalOrders;
         this.menuItems = FXCollections.observableArrayList();
         totalStringProperty = new SimpleStringProperty();
+        subTotalStringProperty = new SimpleStringProperty();
+        salesTaxStringProperty = new SimpleStringProperty();
+
     }
 
-    public String getTotalString () {
-        return totalStringProperty.get();
-    }
 
     public SimpleStringProperty totalStringProperty () {
         return totalStringProperty;
     }
 
-    public void setTotalString () {
-        String string = "$" + new DecimalFormat("###.##").format(total);
-        this.totalStringProperty.set(string);
+    public SimpleStringProperty subTotalStringProperty () {
+        return subTotalStringProperty;
     }
-
     public void addToOrder(MenuItem menuItem) {
         menuItems.add(menuItem);
         total += menuItem.getPrice();
-        setTotalString();
+        setPriceStrings();
+    }
+
+    private void setPriceStrings() {
+        final double SALES_TAX =.0625;
+        DecimalFormat dcFormat = new DecimalFormat("###.##");
+
+        String string = "$" + dcFormat.format(total);
+        this.totalStringProperty.set(string);
+
+        string = "$" + dcFormat.format(total * (1 + SALES_TAX));
+        this.subTotalStringProperty.set(string);
+
+        string = "$" + dcFormat.format(total * SALES_TAX);
+        this.salesTaxStringProperty.set(string);
+
     }
 
     public void removeToOrder(MenuItem menuItem) {
         total -= menuItem.getPrice();
         menuItems.remove(menuItem);
-        setTotalString();
+        setPriceStrings();
     }
 
     public ObservableList<MenuItem> getMenuItems () {
