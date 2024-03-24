e1af6cc8a2be3d5c641f7d0a263efc26ca4f6a48
Francisco <stevenmarquezth18@gmail.com>
2024-03-23 21:58:10 -0400

    added comments

--- src/main/java/rucafe/gui/Order.java
+++ src/main/java/rucafe/gui/Order.java
@@ -6,18 +6,17 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
 import java.text.DecimalFormat;
-import java.util.ArrayList;
-
 
 /**
- * Order class represents the current order;
+ * Order class represents a unique order comprised of menu Items in RUCafe.
+ * User can add and remove menu items from an order.
  */
 public class Order {
     private static int TOTAL_ORDERS = 0;
 
     private int orderNumber;
     private ObservableList<MenuItem> menuItems;
-    private double total; // maybe make into property?
+    private double total;
     private SimpleStringProperty totalStringProperty;
     private SimpleStringProperty subTotalStringProperty;
     private SimpleStringProperty salesTaxStringProperty;
@@ -34,20 +33,52 @@ public class Order {
 
     }
 
-
+    /**
+     * String property that contains a string showing a formatted total of an order.
+     * @return
+     */
     public SimpleStringProperty totalStringProperty () {
         return totalStringProperty;
     }
 
+    /**
+     * @return String property that contains a string showing a formatted sub-total of an order.
+     */
     public SimpleStringProperty subTotalStringProperty () {
         return subTotalStringProperty;
     }
+
+    /**
+     * @return String property that contains a string showing the formatted sales-tax of an order.
+     */
+    public SimpleStringProperty getSalesTaxStringProperty() {
+        return salesTaxStringProperty;
+    }
+
+    /**
+     * Adds a menu Item to the order and increments the menu items price to the total.
+     * @param menuItem Menu item to be added to the order.
+     */
     public void addToOrder(MenuItem menuItem) {
         menuItems.add(menuItem);
         total += menuItem.getPrice();
         setPriceStrings();
     }
 
+    /**
+     * removes a menu Item from order and decrements the menu items price from the order total.
+     * @param menuItem Menu item to be removed from the order.
+     */
+    public void removeFromOrder(MenuItem menuItem) {
+        total -= menuItem.getPrice();
+        menuItems.remove(menuItem);
+        setPriceStrings();
+    }
+
+    /**
+     * Sets up synchronization between the order total and the reflected total on the gui.
+     * This method is called everytime a menu item is added or removed from an order.
+     */
     private void setPriceStrings() {
         final double SALES_TAX =.0625;
         DecimalFormat dcFormat = new DecimalFormat("###.##");
@@ -61,12 +92,6 @@ public class Order {
         string = "$" + dcFormat.format(total * SALES_TAX);
         this.salesTaxStringProperty.set(string);
 
-    }
-
-    public void removeToOrder(MenuItem menuItem) {
-        total -= menuItem.getPrice();
-        menuItems.remove(menuItem);
-        setPriceStrings();
     }
 
     /**
